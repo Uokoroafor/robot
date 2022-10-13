5e84cbdec91c1c016b211e2751c25089793cc812
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2022-10-13 22:21:43 +0100

    Some fine-tuning on the code. More to follow

--- robot.py
+++ robot.py
@@ -1,41 +1,40 @@
 class Robot:
-    def __init__(self,identifier,name,position,direction):
-        self.id=identifier
-        self.name=name
+    def __init__(self, identifier, name, position, direction):
+        self.id = identifier
+        self.name = name
         self.position = position
         self.direction = direction
 
-    def move_forward(self,grid_size):
+    def move_forward(self, grid_size):
         print('Moving one step forward.')
-        if self.direction=='n':
-            new_row=self.position[0]-1
-            new_row=max(0,new_row)
-            self.position=(new_row,self.position[1])
-        elif self.direction=='s':
-            new_row=self.position[0]+1
-            new_row=min(grid_size-1,new_row)
-            self.position=(new_row,self.position[1])
-        elif self.direction=='e':
-            new_col=self.position[1]+1
-            new_col=min(grid_size-1,new_col)
-            self.position=(self.position[0],new_col)
-        elif self.direction=='w':
-            new_col=self.position[1]-1
-            new_col=max(0,new_col)
-            self.position=(self.position[0],new_col)
-        
+        if self.direction == 'n':
+            new_row = self.position[0] - 1
+            new_row = max(0, new_row)
+            self.position = (new_row, self.position[1])
+        elif self.direction == 's':
+            new_row = self.position[0] + 1
+            new_row = min(grid_size - 1, new_row)
+            self.position = (new_row, self.position[1])
+        elif self.direction == 'e':
+            new_col = self.position[1] + 1
+            new_col = min(grid_size - 1, new_col)
+            self.position = (self.position[0], new_col)
+        elif self.direction == 'w':
+            new_col = self.position[1] - 1
+            new_col = max(0, new_col)
+            self.position = (self.position[0], new_col)
 
     def turn_90_degrees(self):
         """ Changes direction 90 degrees"""
         print('Turning 90 degrees clockwise.')
-        if self.direction=='n':
-            self.direction='e'
-        elif self.direction=='s':
-            self.direction='w'
-        elif self.direction=='e':
-            self.direction='s'
-        elif self.direction=='w':
-            self.direction='n'
+        if self.direction == 'n':
+            self.direction = 'e'
+        elif self.direction == 's':
+            self.direction = 'w'
+        elif self.direction == 'e':
+            self.direction = 's'
+        elif self.direction == 'w':
+            self.direction = 'n'
         self.print_location()
 
     def get_direction_string(self):
@@ -48,7 +47,6 @@ class Robot:
             return 'East'
         elif self.direction == 'w':
             return 'West'
-
 
     def print_greeting(self):
         """ Prints my robot's name and id"""
@@ -65,9 +63,9 @@ class Robot:
                 return 'East'
             elif txt == 'w':
                 return 'West'
+
         dir_string = get_direction_string(self.direction)
         print(f'My current location is ({self.position}), facing {dir_string}.')
-
 
     def print_search(self):
         print(f'{self.name} is looking for its drink.')
